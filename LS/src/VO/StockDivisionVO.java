6622f2e43bb089ac68740372c805ba89d6da4afe
CTGG <ctg14@software.nju.edu.cn>
2015-10-26 21:03:59 +0800

    GoodsBLService_Stub

--- LS/src/VO/StockDivisionVO.java
+++ LS/src/VO/StockDivisionVO.java
@@ -8,5 +8,25 @@ package VO;
  *
  */
 public class StockDivisionVO {
-
+	public String cityNum;
+	public String destination;
+	public int block;
+	public int place;
+	public boolean available;
+	
+	/**
+	 * 
+	 * @param cn    id of this warehouse
+	 * @param des   destination of this place
+	 * @param b	    block to which this place belongs
+	 * @param p	    number of this place
+	 * @param avai  state of this place
+	 */
+	public StockDivisionVO(String cn,String des,int b,int p,boolean avai){
+		cityNum=cn;
+		destination=des;
+		block=b;
+		place=p;
+		available=avai;
+	}
 }
