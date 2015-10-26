259d85904a26fa72c62637c6d63d05acfc7d47ec
flyawaygh <776550026@qq.com>
2015-10-26 18:34:05 +0800

    I started the Driver~~

---
+++ LS/src/driver/GoodsBLService_Driver.java
@@ -0,0 +1,15 @@
+package driver;
+
+import blservice.goodsblservice.GoodsBLService;
+
+public class GoodsBLService_Driver {
+
+	GoodsBLService goodsblservice;
+	public GoodsBLService_Driver(GoodsBLService goodsbl) {
+		this.goodsblservice = goodsbl;
+	}
+
+	public void drive(GoodsBLService goodsBLService){
+		
+	}
+}
