ba4ca161f599450410eff4279af79a029e4cbbd0
874855266@qq.com <874855266@qq.com>
2015-11-15 18:39:18 +0800

    toString

--- Logistics-System/src/dataservice/managementdataservice/vehicleanddriverdataservice/VehicleDataService.java
+++ Logistics-System/src/dataservice/managementdataservice/vehicleanddriverdataservice/VehicleDataService.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 import PO.VehiclePO;
 
 public interface VehicleDataService {
-	public void insertVehicle(VehiclePO po)throws RemoteException;
-	public void updateVehicle(VehiclePO po)throws RemoteException;
-	public void deleteVehicle(VehiclePO po)throws RemoteException;
+	public String insertVehicle(VehiclePO po)throws RemoteException;
+	public String updateVehicle(VehiclePO po)throws RemoteException;
+	public String deleteVehicle(VehiclePO po)throws RemoteException;
 	public ArrayList<VehiclePO> showVehicle()throws RemoteException;
 	public VehiclePO findByVehicleNum(String vehicleNum)throws RemoteException;
 	
