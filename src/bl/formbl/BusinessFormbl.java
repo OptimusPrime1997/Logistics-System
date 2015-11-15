3da0126dd9ac84c39f44d02b768592a8dc6fa7c9
flyawaygh <776550026@qq.com>
2015-11-16 06:30:40 +0800

    ~

--- src/bl/formbl/BusinessFormbl.java
+++ src/bl/formbl/BusinessFormbl.java
@@ -1,9 +1,12 @@
 package bl.formbl;
 
+import dataservice.formdataservice.BusinessFormDataService;
+import util.DataFactory;
 import VO.BusinessFormVO;
 import blservice.formblservice.BusinessFormBLService;
 
 public class BusinessFormbl {
+	BusinessFormDataService busiFormdata = DataFactory.getBusinessFormdata();
 
 	public BusinessFormVO show(String startTime, String endTime) {
 		// TODO Auto-generated method stub
