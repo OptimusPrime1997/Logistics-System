c83f521e7c444e34963d42e8f65273f928bc574e
flyawaygh <776550026@qq.com>
2015-11-16 00:07:17 +0800

    controllers

--- src/bl/formbl/ProfitFormbl.java
+++ src/bl/formbl/ProfitFormbl.java
@@ -3,15 +3,13 @@ package bl.formbl;
 import VO.ProfitFormVO;
 import blservice.formblservice.ProfitFormBLService;
 
-public class ProfitFormbl implements ProfitFormBLService{
+public class ProfitFormbl {
 
-	@Override
 	public ProfitFormVO show() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Boolean save(ProfitFormVO vo) {
 		// TODO Auto-generated method stub
 		return null;
