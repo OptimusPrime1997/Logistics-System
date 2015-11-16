17c20a1f2ecccf9bfe9218665da94c3c872e5ffb
flyawaygh <776550026@qq.com>
2015-11-16 19:06:54 +0800

    付款单VO

--- src/VO/PayRepVO.java
+++ src/VO/PayRepVO.java
@@ -1,10 +1,35 @@
 package VO;
 
+import java.util.ArrayList;
+
 public class PayRepVO extends ReceiptVO{
 	public String BankAccountNum;
 	public String PayPersonNum;
-	public String[] PayThings = new String[3];  //申请时间，原因，金额
-	public String[] Salary = new String[3];  //收款人名字、编号；金额
-	public String[] TransferPay = new String[2];  //中转单编号；运费
-	public String[] rent = new String[3];  //收租人名字，手机；金额
+	public ArrayList<littleThing> littleThings;
+	public ArrayList<Salary> Salary ; //收款人名字、编号；金额
+	public ArrayList<TransferPay>TransferPay ;
+	public ArrayList<rent> rent ;	
+	class Salary{
+		String getterName;//收款人名字
+		String getterNum;//收款人编号
+		String date;
+		double money;
+	}
+	class TransferPay{
+		String transferRepNum;//中转单编号
+		String date;
+		double money;
+	}
+	class rent{
+		String getterName;//收租人姓名
+		String phoneNum;
+		double money;
+		String date;
+	}
+	class littleThing{
+		String date;
+		String reason;//付款原因
+		double money;
+	}
+	
 }
