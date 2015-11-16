975c9097c9c71bf17856f786d179c98b109d6f32
flyawaygh <776550026@qq.com>
2015-11-16 09:58:04 +0800

    ~

--- src/VO/CashRepVO.java
+++ src/VO/CashRepVO.java
@@ -1,7 +1,7 @@
 package VO;
 
 public class CashRepVO extends ReceiptVO {
-	int money;
-	String getCourierNum;
+	public int money;
+	public String getCourierNum;
 //	ArrayList<listNum> goods;
 }
--- src/bl/formbl/ProfitFormbl.java
+++ src/bl/formbl/ProfitFormbl.java
@@ -17,13 +17,16 @@ public class ProfitFormbl {
 	ProfitFormDataService profitFormData=DataFactory.getProfitFormdata();
 	
 	public ProfitFormVO show() {
+		double totalIn=0,totalOut=0,totalProfit=0;
 		PayRepblImpl payRep=new PayRepblImpl();
 		CashRepblImpl cashRep=new CashRepblImpl();
-		ArrayList<PayRepVO> moneyIn=payRep.forPay();
-		ArrayList<CashRepVO> moneyOut=cashRep.get();
+		ArrayList<PayRepVO> moneyOut=payRep.forPay();
+		ArrayList<CashRepVO> moneyIn=cashRep.get();
+		for(int i=0;i<moneyIn.size();i++){
+			totalIn+=moneyIn.get(i).money;
+		}
+		//TODO   calculate totalOut with the moneyIn&moneyOut
 		
-		//TODO   calculate totalIn&totalOut with the moneyIn&moneyOut
-		double totalIn=0,totalOut=0,totalProfit=0;
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
 	    ProfitFormVO vo = new ProfitFormVO(df.format(new Date()), totalOut, totalIn, totalProfit);
 		return vo;
