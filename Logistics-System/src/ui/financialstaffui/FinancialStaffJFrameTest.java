f91e69944cb6d344d9ced38df89a5c09af7aaeb1
874855266@qq.com <874855266@qq.com>
2015-11-18 19:53:56 +0800

    uifinancial test

---
+++ Logistics-System/src/ui/financialstaffui/FinancialStaffJFrameTest.java
@@ -0,0 +1,17 @@
+package ui.financialstaffui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FinancialStaffJFrameTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+		FinancialStaffJFrame financialStaffJFrame=new FinancialStaffJFrame();
+		financialStaffJFrame.setVisible(true);
+		assertEquals(1, 1);
+	}
+
+}
