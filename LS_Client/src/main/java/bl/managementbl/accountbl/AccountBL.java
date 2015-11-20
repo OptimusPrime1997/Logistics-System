1cbab675912766dbae6d68d0e38669712995f763
flyawaygh <776550026@qq.com>
2015-11-20 20:30:18 +0800

    .

--- LS_Client/src/main/java/bl/managementbl/accountbl/AccountBL.java
+++ LS_Client/src/main/java/bl/managementbl/accountbl/AccountBL.java
@@ -8,9 +8,70 @@ import VO.LogVO;
 import VO.ManagementVO.AccountVO;
 import blservice.managementblservice.accountblservice.AccountBLService;
 
-public class AccountBL {
+public class AccountBL implements AccountBLService{
 	public ResultMessage createLog(String content){	
 		MockLog log = new MockLog(content);
 		return log.add();		
+	}
+
+	
+	public ResultMessage insert(AccountVO VO) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public ResultMessage update(AccountVO VO) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public ResultMessage delete(AccountVO VO) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public ResultMessage login(String name, String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public ArrayList<AccountVO> show() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public ResultMessage updateMsg(AccountVO vo) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public AccountVO findByName(String name) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public AccountVO findByNum(String num) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public AccountVO findByInstitutionName(String institutionName)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public ResultMessage addLog(LogVO logvo) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	}
--- LS_Client/src/main/java/ui/managerui/ManagerJFrame.java
+++ LS_Client/src/main/java/ui/managerui/ManagerJFrame.java
@@ -14,6 +14,7 @@ public class ManagerJFrame extends javax.swing.JFrame {
     /**
      * Creates new form ManagerJFrame
      */
+
     public ManagerJFrame() {
         initComponents();
     }
