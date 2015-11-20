b6798a029af6422e15285d491c5ca0db39d41589
flyawaygh <776550026@qq.com>
2015-11-20 21:03:04 +0800

    goods  blservice分开

--- LS_Client/src/main/java/bl/goodsbl/controller/GoodsCheckController.java
+++ LS_Client/src/main/java/bl/goodsbl/controller/GoodsCheckController.java
@@ -1,5 +1,13 @@
 package bl.goodsbl.controller;
 
-public class GoodsCheckController {
+import VO.GoodsVO;
+import bl.goodsbl.Goodsbl;
+import blservice.goodsblservice.GoodsCheckBLService;
+
+public class GoodsCheckController implements GoodsCheckBLService{
+	private Goodsbl goodsbl = new Goodsbl();
+	public GoodsVO check(String listNum) {
+		return goodsbl.check(listNum);
+	}
 
 }
