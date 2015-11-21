3e55e9a993abc690881431fcbfb865762fb9b3d5
bismuth1102 <apple@ljc.local>
2015-11-21 12:37:23 +0800

    ？

---
+++ LS_Client/src/main/java/bl/goodsbl/MockConstBL.java
@@ -0,0 +1,15 @@
+package bl.goodsbl;
+
+import java.rmi.RemoteException;
+
+import VO.ManagementVO.ConstVO;
+import bl.managementbl.constbl.ConstBL;
+
+public class MockConstBL extends ConstBL{
+	public ConstVO findByConstName(String constName) throws RemoteException {
+		ConstVO vo=new ConstVO(constName, 2, 5);
+		return vo;
+	}
+
+
+}
