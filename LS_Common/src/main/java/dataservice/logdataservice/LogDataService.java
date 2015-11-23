aa78e309b55c0358c1d9fb0691038f72660a8f15
flyawaygh <776550026@qq.com>
2015-11-23 13:34:39 +0800

    增加ArrayList 的GoodsVO转PO

--- LS_Common/src/main/java/dataservice/logdataservice/LogDataService.java
+++ LS_Common/src/main/java/dataservice/logdataservice/LogDataService.java
@@ -5,13 +5,12 @@ import java.util.ArrayList;
 
 import bl.logbl.LogType;
 import PO.LogPO;
-import VO.LogVO;
 /**
  * add or check the system log
  * @author Administrator
  *
  */
 public interface LogDataService {
-	public ArrayList<LogVO> show(LogType type)throws RemoteException;
+	public ArrayList<LogPO> show(LogType type)throws RemoteException;
 	public void add(LogPO po)throws RemoteException;
 }
