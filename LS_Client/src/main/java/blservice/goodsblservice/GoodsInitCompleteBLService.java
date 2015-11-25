fb4fd2f43504e1edc4fb2812abe44a3f6e478ac0
flyawaygh <776550026@qq.com>
2015-11-25 17:38:07 +0800

    goodsControllers

---
+++ LS_Client/src/main/java/blservice/goodsblservice/GoodsInitCompleteBLService.java
@@ -0,0 +1,13 @@
+package blservice.goodsblservice;
+
+import VO.GoodsVO;
+
+public interface GoodsInitCompleteBLService {
+	/**
+	 * return the fare calculated GoodsVO
+	 * @param vo
+	 * @return
+	 */
+	public GoodsVO getInitCompleteGoods(GoodsVO vo);
+
+}
