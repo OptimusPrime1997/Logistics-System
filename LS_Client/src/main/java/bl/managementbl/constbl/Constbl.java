4eab08f4b36ca56f5c09e65b152a771ac2fa518a
flyawaygh <776550026@qq.com>
2015-11-25 08:41:04 +0800

    。

--- LS_Client/src/main/java/bl/managementbl/constbl/Constbl.java
+++ LS_Client/src/main/java/bl/managementbl/constbl/Constbl.java
@@ -1,16 +1,16 @@
 package bl.managementbl.constbl;
-
 import java.rmi.RemoteException;
-import java.util.ArrayList;
 
+import util.enumData.Const;
 import util.enumData.ResultMessage;
 import VO.ManagementVO.ConstVO;
-import bl.managementbl.accountbl.MockLog;
-import blservice.managementblservice.constblservice.ConstBLService;
-
 public class Constbl {
 	public ResultMessage createLog(String content){	
 		MockLog log = new MockLog(content);
 		return log.add();		
+	}
+	public ConstVO findByConstName(Const constName) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
