81507c977f670b78affec1ba5d0b683b99148740
874855266@qq.com <874855266@qq.com>
2015-11-25 13:13:59 +0800

    删掉之前junit测试

--- LS_Client/src/main/java/bl/managementbl/salarypolicybl/SalaryPolicybl.java
+++ LS_Client/src/main/java/bl/managementbl/salarypolicybl/SalaryPolicybl.java
@@ -3,12 +3,7 @@ package bl.managementbl.salarypolicybl;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
-import util.enumData.ResultMessage;
 import VO.ManagementVO.SalaryPolicyVO;
-import bl.managementbl.accountbl.MockLog;
 import blservice.managementblservice.salarypolicyblservice.SalaryPolicyBLService;
 
-public class SalaryPolicybl{public ResultMessage createLog(String content){	
-	MockLog log = new MockLog(content);
-	return log.add();		
-}}
+public class SalaryPolicybl{}
