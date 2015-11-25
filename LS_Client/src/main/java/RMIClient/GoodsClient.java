ee6796d2937ef3dba784e04c34c42c27df019556
flyawaygh <776550026@qq.com>
2015-11-25 20:03:52 +0800

    remotes~~

--- LS_Client/src/main/java/RMIClient/GoodsClient.java
+++ LS_Client/src/main/java/RMIClient/GoodsClient.java
@@ -1,17 +1,21 @@
 package RMIClient;
-
+import java.rmi.Naming;
 import java.rmi.RemoteException;
-
+import RMIservice.GoodsDataRemote;
 import dataservice.goodsdataservice.GoodsDataService;
 
 public class GoodsClient {
+	GoodsDataRemote remote;
 	public void getInstance(){
-		//TODO
+		try {
+			remote=(GoodsDataRemote)Naming.lookup("rmi://127.0.0.1:19006/goods");
+		} catch (Exception e) {
+		}
 	}
 	
 	public GoodsDataService getGoodsDataService() throws RemoteException{
 		getInstance();
-		return null;
+		return remote.getGoodsDataService();
 	}
 
 }
