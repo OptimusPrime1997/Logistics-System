c8a1c6099cd70ee170cf0727584bbad984f9be02
flyawaygh <776550026@qq.com>
2015-11-25 08:40:12 +0800

    .

--- LS_Client/src/main/java/bl/managementbl/institutionbl/Institutionbl.java
+++ LS_Client/src/main/java/bl/managementbl/institutionbl/Institutionbl.java
@@ -1,15 +1,7 @@
-
-
-
 package bl.managementbl.institutionbl;
-
-import java.rmi.RemoteException;
-import java.util.ArrayList;
 
 import util.enumData.ResultMessage;
 import bl.managementbl.accountbl.MockLog;
-import blservice.managementblservice.institutionblservice.InstitutionBLService;
-import VO.ManagementVO.InstitutionVO;
 
 
 public class Institutionbl  {public ResultMessage createLog(String content){	
