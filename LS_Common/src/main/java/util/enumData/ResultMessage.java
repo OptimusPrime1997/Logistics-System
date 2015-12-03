3e53941b0fffc5f8569e5aae74445792a624f94a
bismuth1102 <apple@ljc.local>
2015-12-03 19:56:32 +0800

    ok

--- LS_Common/src/main/java/util/enumData/ResultMessage.java
+++ LS_Common/src/main/java/util/enumData/ResultMessage.java
@@ -11,32 +11,6 @@ public enum ResultMessage {
 	SUCCESS, //成功
 	FAILED,	//失败
 	WRONG,	//错误
-<<<<<<< HEAD
-	LINK_FAILURE,//连接错误
-	EXIST,	//已存在
-	NOT_FOUND,	//没找到对应结果
-	
-	REPNUM_LENGTH_OVER,	//编号过长
-	REPNUM_LENGTH_LACKING,	//编号过短
-	
-	PHONE_LENGTH_WRONG,//手机号长度有误
-	UNVALID_CHAR,	//含非法字符
-	UNREASONABLE_GOODS_NUM,//不合理的内件数
-	UNREASONABLE_WEIGHT_NUM,//不合理的重量数值
-	UNREASONABLE_VOLUME_NUM,//不合理的体积数值
-	NOT_COMPLETED,	//未填写
-	
-	WRONG_ACCOUNTNUM,	//账号格式错误
-	NOT_FOUND_ACCOUNTNUM,//账号不存在
-	WRONG_PASSWORD,//错误密码
-
-
-	NOT_FOUND_FILE,//文件未找到
-	IOFAILED,//文件读写失败
-	INPUT_SHOULD_BE_POSITIVE_INTEGER, //输入应该为正整数
-	NUMBER_OVER,   //输入数值太大
-	NOT_AVAILABLE; //该库存区位已被占用
-=======
 	/**
 	 * 连接错误
 	 */
@@ -129,7 +103,6 @@ public enum ResultMessage {
 	 * 该库存区位已被占用
 	 */
 	NOT_AVAILABLE; 
->>>>>>> origin/master
 	
 	public static String toFriendlyString(ResultMessage resultMessage) {
 		
