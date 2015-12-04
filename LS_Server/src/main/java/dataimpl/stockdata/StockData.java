56cf397322ac4a0563758855f24e9cd7999b6134
CTGG <ctg14@software.nju.edu.cn>
2015-12-04 21:33:43 +0800

    继续完成update

--- LS_Server/src/main/java/dataimpl/stockdata/StockData.java
+++ LS_Server/src/main/java/dataimpl/stockdata/StockData.java
@@ -4,22 +4,20 @@
 package dataimpl.stockdata;
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 
 import util.enumData.ResultMessage;
 import PO.StockPO;
+import PO.ReceiptPO.InStockPO;
 import PO.ReceiptPO.InStockRepPO;
 import PO.ReceiptPO.OutStockRepPO;
 import dataservice.stockdataservice.StockDataService;
-import dataservice.stockdataservice.StockInitialDataService;
+
 import datautil.DataUtility;
 
 /**
@@ -50,7 +48,6 @@ public class StockData extends UnicastRemoteObject implements StockDataService{
 	@Override
 	public ResultMessage add(StockPO po) throws RemoteException {
 		ArrayList<Object> list;
-		ResultMessage rm;
 		try {
 			list = du.getAll(filename);
 			
@@ -63,7 +60,7 @@ public class StockData extends UnicastRemoteObject implements StockDataService{
 				}
 			}
 			
-			return rm = du.save(po, filename);
+			return du.save(po, filename);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} 
@@ -129,8 +126,22 @@ public class StockData extends UnicastRemoteObject implements StockDataService{
 	 */
 	@Override
 	public ResultMessage update(InStockRepPO po) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		ResultMessage rm = ResultMessage.SUCCESS;
+		String inrepnum = po.getNum();
+		String date = po.getDate();
+		//TODO 得到本地城市
+		String citynum = "Nanjing";
+		ArrayList<InStockPO> list = po.getInStockPOs();
+		for(InStockPO inpo : list) {
+			int block = Integer.parseInt(inpo.getArea());
+			int place = Integer.parseInt(inpo.getLoc());
+			//TODO 由区号得到城市号，暂时只用了区号代替城市
+			rm = add(new StockPO(citynum, inpo.getOrder(), inrepnum, date, inpo.getArea(), block, place));
+			if(!rm.equals(ResultMessage.SUCCESS)) {
+				return ResultMessage.FAILED;
+			}
+		}
+		return rm;
 	}
 
 
@@ -139,8 +150,15 @@ public class StockData extends UnicastRemoteObject implements StockDataService{
 	 */
 	@Override
 	public ResultMessage update(OutStockRepPO po) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		ResultMessage rm = ResultMessage.SUCCESS;
+		ArrayList<String> list = po.getGoods();
+		for(String goods : list) {
+			 rm = delete(goods);
+			 if(!rm.equals(ResultMessage.SUCCESS)) {
+				 return ResultMessage.FAILED;
+			 }
+		}
+		return rm;
 	}
 
 	
--- LS_Server/src/main/java/dataimpl/stockdata/StockDivisionData.java
+++ LS_Server/src/main/java/dataimpl/stockdata/StockDivisionData.java
@@ -3,6 +3,7 @@
  */
 package dataimpl.stockdata;
 
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
@@ -11,6 +12,8 @@ import javax.naming.spi.DirStateFactory.Result;
 
 import util.enumData.ResultMessage;
 import PO.StockDivisionPO;
+import PO.StockPO;
+import PO.ReceiptPO.InStockPO;
 import PO.ReceiptPO.InStockRepPO;
 import PO.ReceiptPO.OutStockRepPO;
 import dataservice.stockdataservice.StockDivisionDataService;
@@ -45,12 +48,53 @@ public class StockDivisionData extends UnicastRemoteObject implements StockDivis
 	
 	@Override
 	public ResultMessage update(InStockRepPO po) throws RemoteException {
-		return null;
-		// TODO Auto-generated method stub
+		ResultMessage rm = ResultMessage.SUCCESS;
+		//TODO 得到本地城市
+		String citynum = "Nanjing";
+		ArrayList<InStockPO> list = po.getInStockPOs();
+		for(InStockPO inpo : list) {
+			int block = Integer.parseInt(inpo.getArea());
+			int place = Integer.parseInt(inpo.getLoc());
+			//TODO 由区号得到城市号，暂时只用了区号代替城市
+			rm = add(new StockDivisionPO(citynum, block+"", block, place));
+			if(!rm.equals(ResultMessage.SUCCESS)) {
+				return ResultMessage.FAILED;
+			}
+		}
+		return rm;
 		
 	}
 
 	
+	/**
+	 * @param stockDivisionPO
+	 * @return
+	 */
+	private ResultMessage add(StockDivisionPO po) {
+		// TODO 返回值还没完成
+		
+
+		ArrayList<Object> list;
+		try {
+			list = du.getAll(filename);
+			
+			for(Object o:list){
+				StockDivisionPO p = (StockDivisionPO)o;
+				
+				if(p.getBlock()==po.getBlock()&&p.getPlace()==po.getPlace()){
+					return ResultMessage.EXIST;
+				}
+			}
+			
+			return du.save(po, filename);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		return ResultMessage.FAILED;
+	}
+
+
 	@Override
 	public ResultMessage update(OutStockRepPO po) throws RemoteException {
 		return null;
@@ -60,9 +104,23 @@ public class StockDivisionData extends UnicastRemoteObject implements StockDivis
 
 	
 	@Override
-	public ArrayList<StockDivisionPO> getStockDivision() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<StockDivisionPO> getStockDivision() throws IOException {
+	 
+		ArrayList<StockDivisionPO> list = new ArrayList<StockDivisionPO>();
+		ArrayList<Object> listo;
+		try {
+			listo = du.getAll(filename);
+			
+			for(Object o:listo) {
+				StockDivisionPO po = (StockDivisionPO) o;
+				list.add(po);
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+		
 	}
 
 }
