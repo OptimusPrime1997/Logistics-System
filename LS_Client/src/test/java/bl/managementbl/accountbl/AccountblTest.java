9a4b1d7e02fea49142b46716cc8fc63388e596ae
bismuth1102 <562915475@qq.com>
2016-01-05 01:44:52 +0800

    撒旦撒旦

--- LS_Client/src/test/java/bl/managementbl/accountbl/AccountblTest.java
+++ LS_Client/src/test/java/bl/managementbl/accountbl/AccountblTest.java
@@ -24,172 +24,125 @@ public class AccountblTest {
 		AccountVO vo = null;
 		AccountVO vo1 = null;
 		try {
-			rmsg = accountbl.add(new AccountVO("02500101001", "王二", "1111",
-					Sex.Sex_MALE, Authority.MANAGER, "12345672501", "025001"));
+			rmsg = accountbl.add(new AccountVO("02500001001", "南总理", "1111", Sex.Sex_MALE, Authority.MANAGER,
+					"12345672501", "025000"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("02500102001", "周季", "1111",
-					Sex.Sex_MALE, Authority.FINANCIALSTAFF_C, "12345672502",
-					"025002"));
+			rmsg = accountbl.add(new AccountVO("02500002001", "南普财", "1111", Sex.Sex_MALE, Authority.FINANCIALSTAFF_C,
+					"12345672502", "025000"));
 			rmsgs.add(rmsg);
-
-			rmsg = accountbl.add(new AccountVO("02500203001", "王石", "1111",
-					Sex.Sex_MALE, Authority.BUSSINESSOFFICER, "12345672503",
-					"025002"));
+			rmsg = accountbl.add(new AccountVO("02500103001", "南营一", "1111", Sex.Sex_MALE, Authority.BUSSINESSOFFICER,
+					"12345672503", "025001"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("02500303001", "王一", "1111",
-					Sex.Sex_MALE, Authority.BUSSINESSOFFICER, "12345672510",
-					"025003"));
+			rmsg = accountbl.add(new AccountVO("02500203001", "南营二", "1111", Sex.Sex_MALE, Authority.BUSSINESSOFFICER,
+					"12345672510", "025002"));
 			rmsgs.add(rmsg);
-
-			rmsg = accountbl.add(new AccountVO("02500104001", "万科", "1111",
-					Sex.Sex_MALE, Authority.TRANSFERCTROFFICER, "12345672504",
-					"025001"));
+			rmsg = accountbl.add(new AccountVO("02500004001", "南中转", "1111", Sex.Sex_MALE, Authority.TRANSFERCTROFFICER,
+					"12345672504", "025000"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("02500105001", "雷八", "1111",
-					Sex.Sex_MALE, Authority.WAREHOUSEMAN, "12345672505",
-					"025001"));
+			rmsg = accountbl.add(new AccountVO("02500005001", "南仓库", "1111", Sex.Sex_MALE, Authority.WAREHOUSEMAN,
+					"12345672505", "025000"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl
-					.add(new AccountVO("02500206001", "周五", "1111",
-							Sex.Sex_FAMALE, Authority.COURIER, "12345672506",
-							"025002"));
-			rmsg = accountbl
-					.add(new AccountVO("02500306001", "赵云", "1111",
-							Sex.Sex_FAMALE, Authority.COURIER, "12345672511",
-							"025003"));
-			rmsg = accountbl.add(new AccountVO("02500108001", "张天", "1111",
-					Sex.Sex_FAMALE, Authority.ADMINISTRATOR, "12345672511",
-					"025001"));
+			rmsg = accountbl.add(new AccountVO("02500106001", "南快一", "1111", Sex.Sex_FAMALE, Authority.COURIER,
+					"12345672506", "025001"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("02500109001", "王东", "1111",
-					Sex.Sex_MALE, Authority.FINANCIALSTAFF_V, "12345672509",
-					"025001"));
+			rmsg = accountbl.add(new AccountVO("02500206001", "南快二", "1111", Sex.Sex_FAMALE, Authority.COURIER,
+					"12345672511", "025002"));
 			rmsgs.add(rmsg);
-
-			rmsg = accountbl.add(new AccountVO("02100101001", "赵一", "1111",
-					Sex.Sex_MALE, Authority.MANAGER, "12345672101", "021001"));
+			rmsg = accountbl.add(new AccountVO("02500008001", "南管理", "1111", Sex.Sex_FAMALE, Authority.ADMINISTRATOR,
+					"12345672511", "025000"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("02100102101", "赵二", "1111",
-					Sex.Sex_MALE, Authority.FINANCIALSTAFF_C, "12345672102",
-					"021002"));
+			rmsg = accountbl.add(new AccountVO("02500009001", "南高财", "1111", Sex.Sex_MALE, Authority.FINANCIALSTAFF_V,
+					"12345672509", "025000"));
 			rmsgs.add(rmsg);
-
-			rmsg = accountbl.add(new AccountVO("02100203001", "赵三", "1111",
-					Sex.Sex_MALE, Authority.BUSSINESSOFFICER, "12345672103",
-					"021002"));
+			rmsg = accountbl.add(new AccountVO("02100001001", "上总理", "1111", Sex.Sex_MALE, Authority.MANAGER,
+					"12345672101", "021000"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("02100303001", "赵四", "1111",
-					Sex.Sex_MALE, Authority.BUSSINESSOFFICER, "12345672110",
-					"021003"));
+			rmsg = accountbl.add(new AccountVO("02100002001", "上普财", "1111", Sex.Sex_MALE, Authority.FINANCIALSTAFF_C,
+					"12345672102", "021000"));
 			rmsgs.add(rmsg);
-
-			rmsg = accountbl.add(new AccountVO("02100104001", "赵五", "1111",
-					Sex.Sex_MALE, Authority.TRANSFERCTROFFICER, "12345672104",
-					"021001"));
+			rmsg = accountbl.add(new AccountVO("02100103001", "上营一", "1111", Sex.Sex_MALE, Authority.BUSSINESSOFFICER,
+					"12345672103", "021001"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("02100105001", "赵六", "1111",
-					Sex.Sex_MALE, Authority.WAREHOUSEMAN, "12345672105",
-					"021001"));
+			rmsg = accountbl.add(new AccountVO("02100203001", "上营二", "1111", Sex.Sex_MALE, Authority.BUSSINESSOFFICER,
+					"12345672110", "021002"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl
-					.add(new AccountVO("02100206001", "赵七", "1111",
-							Sex.Sex_FAMALE, Authority.COURIER, "12345672106",
-							"021002"));
-			rmsg = accountbl
-					.add(new AccountVO("02100306001", "赵八", "1111",
-							Sex.Sex_FAMALE, Authority.COURIER, "12345672111",
-							"021003"));
-			rmsg = accountbl.add(new AccountVO("02100108001", "赵九", "1111",
-					Sex.Sex_FAMALE, Authority.ADMINISTRATOR, "12345672111",
-					"021001"));
+			rmsg = accountbl.add(new AccountVO("02100004001", "上中转", "1111", Sex.Sex_MALE, Authority.TRANSFERCTROFFICER,
+					"12345672104", "021000"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("02100109001", "赵十", "1111",
-					Sex.Sex_MALE, Authority.FINANCIALSTAFF_V, "12345672109",
-					"021001"));
+			rmsg = accountbl.add(new AccountVO("02100005001", "上仓库", "1111", Sex.Sex_MALE, Authority.WAREHOUSEMAN,
+					"12345672105", "021000"));
 			rmsgs.add(rmsg);
-
-			rmsg = accountbl.add(new AccountVO("02000101001", "方一", "1111",
-					Sex.Sex_MALE, Authority.MANAGER, "12345672001", "020001"));
+			rmsg = accountbl.add(new AccountVO("02100106001", "上快一", "1111", Sex.Sex_FAMALE, Authority.COURIER,
+					"12345672106", "021001"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("02000102001", "方二", "1111",
-					Sex.Sex_MALE, Authority.FINANCIALSTAFF_C, "12345672002",
-					"020002"));
+			rmsg = accountbl.add(new AccountVO("02100206001", "上快二", "1111", Sex.Sex_FAMALE, Authority.COURIER,
+					"12345672111", "021002"));
 			rmsgs.add(rmsg);
-
-			rmsg = accountbl.add(new AccountVO("02000203001", "方三", "1111",
-					Sex.Sex_MALE, Authority.BUSSINESSOFFICER, "12345672003",
-					"020002"));
+			rmsg = accountbl.add(new AccountVO("02100008001", "上管理", "1111", Sex.Sex_FAMALE, Authority.ADMINISTRATOR,
+					"12345672111", "021000"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("02000303001", "方四", "1111",
-					Sex.Sex_MALE, Authority.BUSSINESSOFFICER, "12345672010",
-					"020003"));
+			rmsg = accountbl.add(new AccountVO("02100009001", "上高财", "1111", Sex.Sex_MALE, Authority.FINANCIALSTAFF_V,
+					"12345672109", "021000"));
 			rmsgs.add(rmsg);
-
-			rmsg = accountbl.add(new AccountVO("02000104001", "方五", "1111",
-					Sex.Sex_MALE, Authority.TRANSFERCTROFFICER, "12345672004",
-					"020001"));
+			rmsg = accountbl.add(new AccountVO("02000001001", "广总理", "1111", Sex.Sex_MALE, Authority.MANAGER,
+					"12345672001", "020000"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("02000105001", "方六", "1111",
-					Sex.Sex_MALE, Authority.WAREHOUSEMAN, "12345672005",
-					"020001"));
+			rmsg = accountbl.add(new AccountVO("02000002001", "广普财", "1111", Sex.Sex_MALE, Authority.FINANCIALSTAFF_C,
+					"12345672002", "020000"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl
-					.add(new AccountVO("02000206001", "方七", "1111",
-							Sex.Sex_FAMALE, Authority.COURIER, "12345672006",
-							"020002"));
-			rmsg = accountbl
-					.add(new AccountVO("02000306001", "方八", "1111",
-							Sex.Sex_FAMALE, Authority.COURIER, "12345672011",
-							"020003"));
-			rmsg = accountbl.add(new AccountVO("02000108001", "方九", "1111",
-					Sex.Sex_FAMALE, Authority.ADMINISTRATOR, "12345672011",
-					"020001"));
+			rmsg = accountbl.add(new AccountVO("02000103001", "广营一", "1111", Sex.Sex_MALE, Authority.BUSSINESSOFFICER,
+					"12345672003", "020001"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("02000109001", "方十", "1111",
-					Sex.Sex_MALE, Authority.FINANCIALSTAFF_V, "12345672009",
-					"020001"));
+			rmsg = accountbl.add(new AccountVO("02000203001", "广营二", "1111", Sex.Sex_MALE, Authority.BUSSINESSOFFICER,
+					"12345672010", "020002"));
 			rmsgs.add(rmsg);
-
-			rmsg = accountbl.add(new AccountVO("01000101001", "胡一", "1111",
-					Sex.Sex_MALE, Authority.MANAGER, "12345671001", "010001"));
+			rmsg = accountbl.add(new AccountVO("02000004001", "广中转", "1111", Sex.Sex_MALE, Authority.TRANSFERCTROFFICER,
+					"12345672004", "020000"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("01000101001", "胡二", "1111",
-					Sex.Sex_MALE, Authority.FINANCIALSTAFF_C, "12345671002",
-					"010002"));
+			rmsg = accountbl.add(new AccountVO("02000005001", "广仓库", "1111", Sex.Sex_MALE, Authority.WAREHOUSEMAN,
+					"12345672005", "020000"));
 			rmsgs.add(rmsg);
-
-			rmsg = accountbl.add(new AccountVO("01000103001", "胡三", "1111",
-					Sex.Sex_MALE, Authority.BUSSINESSOFFICER, "12345671003",
-					"010002"));
+			rmsg = accountbl.add(new AccountVO("02000106001", "广快一", "1111", Sex.Sex_FAMALE, Authority.COURIER,
+					"12345672006", "020001"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("01000303001", "胡四", "1111",
-					Sex.Sex_MALE, Authority.BUSSINESSOFFICER, "12345671010",
-					"010003"));
+			rmsg = accountbl.add(new AccountVO("02000206001", "广快二", "1111", Sex.Sex_FAMALE, Authority.COURIER,
+					"12345672011", "020002"));
 			rmsgs.add(rmsg);
-
-			rmsg = accountbl.add(new AccountVO("01000104001", "胡五", "1111",
-					Sex.Sex_MALE, Authority.TRANSFERCTROFFICER, "12345671004",
-					"010001"));
+			rmsg = accountbl.add(new AccountVO("02000008001", "广管理", "1111", Sex.Sex_FAMALE, Authority.ADMINISTRATOR,
+					"12345672011", "020000"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("01000105001", "胡六", "1111",
-					Sex.Sex_MALE, Authority.WAREHOUSEMAN, "12345671005",
-					"010001"));
+			rmsg = accountbl.add(new AccountVO("02000009001", "广高财", "1111", Sex.Sex_MALE, Authority.FINANCIALSTAFF_V,
+					"12345672009", "020000"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl
-					.add(new AccountVO("01000106001", "胡七", "1111",
-							Sex.Sex_FAMALE, Authority.COURIER, "12345671006",
-							"010002"));
-			rmsg = accountbl
-					.add(new AccountVO("01000306001", "胡八", "1111",
-							Sex.Sex_FAMALE, Authority.COURIER, "12345671011",
-							"010003"));
-			rmsg = accountbl.add(new AccountVO("01000108001", "胡九", "1111",
-					Sex.Sex_FAMALE, Authority.ADMINISTRATOR, "12345671011",
-					"010001"));
+			rmsg = accountbl.add(new AccountVO("01000001001", "北总理", "1111", Sex.Sex_MALE, Authority.MANAGER,
+					"12345671001", "010000"));
 			rmsgs.add(rmsg);
-			rmsg = accountbl.add(new AccountVO("01000109001", "胡十", "1111",
-					Sex.Sex_MALE, Authority.FINANCIALSTAFF_V, "12345671009",
-					"010001"));
+			rmsg = accountbl.add(new AccountVO("01000001001", "北普财", "1111", Sex.Sex_MALE, Authority.FINANCIALSTAFF_C,
+					"12345671002", "010000"));
+			rmsgs.add(rmsg);
+			rmsg = accountbl.add(new AccountVO("01000103001", "北营一", "1111", Sex.Sex_MALE, Authority.BUSSINESSOFFICER,
+					"12345671003", "010001"));
+			rmsgs.add(rmsg);
+			rmsg = accountbl.add(new AccountVO("01000203001", "北营二", "1111", Sex.Sex_MALE, Authority.BUSSINESSOFFICER,
+					"12345671010", "010002"));
+			rmsgs.add(rmsg);
+			rmsg = accountbl.add(new AccountVO("01000004001", "北中转", "1111", Sex.Sex_MALE, Authority.TRANSFERCTROFFICER,
+					"12345671004", "010000"));
+			rmsgs.add(rmsg);
+			rmsg = accountbl.add(new AccountVO("01000005001", "北仓库", "1111", Sex.Sex_MALE, Authority.WAREHOUSEMAN,
+					"12345671005", "010000"));
+			rmsgs.add(rmsg);
+			rmsg = accountbl.add(new AccountVO("01000106001", "北快一", "1111", Sex.Sex_FAMALE, Authority.COURIER,
+					"12345671006", "010001"));
+			rmsgs.add(rmsg);
+			rmsg = accountbl.add(new AccountVO("01000206001", "北快二", "1111", Sex.Sex_FAMALE, Authority.COURIER,
+					"12345671011", "010002"));
+			rmsgs.add(rmsg);
+			rmsg = accountbl.add(new AccountVO("01000008001", "北管理", "1111", Sex.Sex_FAMALE, Authority.ADMINISTRATOR,
+					"12345671011", "010000"));
+			rmsgs.add(rmsg);
+			rmsg = accountbl.add(new AccountVO("01000009001", "北高财", "1111", Sex.Sex_MALE, Authority.FINANCIALSTAFF_V,
+					"12345671009", "010000"));
 			rmsgs.add(rmsg);
 			for (int i = 0; i < rmsgs.size(); i++) {
 				System.out.println(rmsgs.get(i));
